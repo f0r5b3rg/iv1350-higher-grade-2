692b502e607d8fa1f79b379179757ef3f0db843a
rdesta1 <rmdesta52@gmail.com>
2026-05-21 00:21:21 +0200

    cleanup

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,30 +1,25 @@
 package main;
+import implementation.InheritanceRandom;
 import implementation.CompositionCurrency;
 
 public class Main
 {
-
     public static void main(String[] args)
     {
-        /*
-        InheritanceRandom inheritanceRandom = new InheritanceRandom();
-        inheritanceRandom.drawCard(4);
-        */
-
-
-       CompositionCurrency cc = new CompositionCurrency("mxn");
-       try
-       {
-           cc.currentExchangeRateForSek();
-       }
-       catch(Exception e)
-       {
-           System.out.println("Unexpected issues " + e);
-       }
-
-
+        InheritanceRandom random = new InheritanceRandom();
+        CompositionCurrency currency = new CompositionCurrency("mxn");
+       
+        System.out.println("Inheritance test \n");
+        random.drawCard(4);
+    
+        System.out.println("Composition test \n"); 
+        try
+        {
+            currency.currentExchangeRateForSek();
+        }
+        catch(Exception e)
+        {
+            System.out.println("Unexpected issues " + e);
+        }
     }
 }
-
-
-
